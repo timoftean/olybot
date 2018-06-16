f57f881e1fe52ebe6fa67b4af9365ce1262bc5a1
Nicu <timoftealexandru5@gmail.com>
2018-06-17 00:36:51 +0300

    handle env

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,7 +1,19 @@
+import * as fs from 'fs'
 import * as dotenv from 'dotenv'
-const result = dotenv.config()
-if (result.error) {
-    throw result.error
+
+let envConfig = null
+if (process.env.NODE_ENV === 'test') {
+    envConfig = dotenv.parse(fs.readFileSync('.env.test'))
+} else if (process.env.NODE_ENV === 'production') {
+    envConfig = dotenv.parse(fs.readFileSync('.env.production'))
+} else {
+    envConfig = dotenv.parse(fs.readFileSync('.env'))
+}
+
+for (const k in envConfig) {
+    if (k) {
+        process.env[k] = envConfig[k]
+    }
 }
 
 import { Server } from './server'
